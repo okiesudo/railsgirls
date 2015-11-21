95558fde1db39c04c1e9ece99491a651840d9355
sudookie <sudookie@sudookie-no-MacBook-Air-3.local>
2015-11-21 15:54:24 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :ideas
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root to: redirect('/ideas')
+  get "pages/info"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
